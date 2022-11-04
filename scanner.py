bbeeb186126b1dc6d0ab652e1f888844f1ff6393
Math-x <97481289+Math-x-io@users.noreply.github.com>
2022-11-04 01:45:57 +0100

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+import socket
+
+adresseip = input("Entrez une adresse IP (format IPV4) :")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4 TCP
+for port in range(1, 100):
+	if sock.connect_ex((adresseip, port)):
+		print ("Le port", port, "est fermé")
+	else:
+		if port==80 :
+			print("Le port", port, " HTTP est ouvert")
+		elif port == 25 : 
+			print("Le port", port, " SMTP est ouvert")
+		elif port == 22 : 
+			print("Le port", port, " SSH est ouvert")
+		elif port == 20 or port == 21 : 
+			print("Le port", port, " FTP est ouvert")
+		elif port == 443 : 
+			print("Le port", port, " HTTPS est ouvert")
+		else :
+			print ("Le port", port, "est ouvert")
+
