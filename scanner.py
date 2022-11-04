3c3ddad96b78e6d978aaa921990884bdb8eb2f3d
Math-x <97481289+Math-x-io@users.noreply.github.com>
2022-11-04 02:00:30 +0100

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -1,9 +1,11 @@
 # coding:utf-8
 import socket
 
-adresseip = input("Entrez une adresse IP (format IPV4) :")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4 TCP
-for port in range(1, 100):
+adresseip = input("Entrez une adresse IP (format IPV4) : ")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4 TCPpo
+portFin = input("Jusqu'à quel port souhaitez vous aller lors de votre scan ? (max 1000) : ")
+
+for port in range(1, int(portFin)+1):
 	if sock.connect_ex((adresseip, port)):
 		print ("Le port", port, "est fermé")
 	else:
